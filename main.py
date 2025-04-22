af59681230d66a5256bc80c1cb6559a5aaf46490
FPassarinho <fmapassarinho@gmail.com>
2025-04-22 14:17:18 +0100

    App developement folder organization

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import eel
 
-eel.init('view')  # 'web' is the folder with your HTML files
+eel.init('static_web_folder')  
 
 @eel.expose
 def say_hello_py(x):
   print(f"Hello from JS: {x}")
 
-eel.start('index.html')
+###Ver se ínicio como aplicação de Desktop ou web
+eel.start('html/main_page.html', mode='chrome-app', port=8080, cmdline_args=['--start-fullscreen'])
---
+++ static_web_folder/js/main.js
@@ -0,0 +1,3 @@
+function callPython() {
+  eel.say_hello_py("Hello from JS!")();
+}
