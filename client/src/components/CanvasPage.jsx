dc8caa07ad9b015a19117e21081fcc3fa9bd7e84
FPassarinho <fmapassarinho@gmail.com>
2025-10-02 13:11:10 +0100

    changes

--- client/src/components/CanvasPage.jsx
+++ client/src/components/CanvasPage.jsx
@@ -1,10 +1,43 @@
 import "./canvasPage.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
+import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CanvasPage = () => {
   const navigate = useNavigate();
+  const canvasRef = useRef(null);
+  const [drawing, setDrawing] = useState(false);
+
+  const startDrawing = (e) => {
+    setDrawing(true);
+    draw(e);
+  };
+
+  const endDrawing = () => {
+    setDrawing(false);
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.beginPath();
+  };
+
+  const draw = (e) => {
+    if (!drawing) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const rect = canvas.getBoudingClientRect();
+
+    const x = e.clientx - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineWidth = 8;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "balck";
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  };
 
   return (
     <>
@@ -19,11 +52,16 @@ const CanvasPage = () => {
             </button>
           </div>
           <h2>Draw the letter that you want to test</h2>
-          <canvas className="canvas" />
+          <canvas
+            className="canvas"
+            ref={canvasRef}
+            onMouseDown={startDrawing}
+            onMouseUp={endDrawing}
+            onMouseMove={draw}
+            onMouseLeave={endDrawing}
+          />
           <div className="canvas-buttons">
             <button>Clear</button>
-            <button>Pencil</button>
-            <button>Rubber</button>
             <button>Save</button>
           </div>
         </div>
